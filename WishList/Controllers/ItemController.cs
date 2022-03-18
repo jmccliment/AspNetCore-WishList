31105b16d697c05c00b87ee84cfe33952816c635
Jeremy McCliment <jeremy.mccliment.leum@statefarm.com>
2022-03-18 16:38:06 -0700

    6.1: Create ItemController

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
